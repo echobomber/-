c59523cc12506b8544d689135c6de9df3837843c
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2020-11-28 01:20:20 +0800

    西餐廳切版完成

--- js/all.js
+++ js/all.js
@@ -1,6 +1,39 @@
 $(document).ready(function() {
-    $('.Menu-toggle').on('click',  function(e){
-       e.preventDefault();
-       $('.topNav').toggleClass('openMenu');
-   });
- });
+  //Menu
+  $('.Menu-toggle').on('click',  function(e){
+    e.preventDefault();
+    $('.topNav').toggleClass('openMenu');
+  });
+   //滾動
+  $('.scrollSpy').on('click', function(e){
+    e.preventDefault();
+    let target = $(this).attr('href');
+    //指定 id 距離最頂部多少
+    let targetPos = $(target).offset().top;
+    //為了支援瀏覽器所以要同時下 html,body
+    $('html, body').animate({scrollTop: targetPos-80}, 500);
+    $('.topNav').removeClass('openMenu');
+  });
+  $('.scrollTop').on('click', function(e){
+    $('html, body').animate({scrollTop: 0}, 500);
+  });
+  //每次滾動的時候執行
+  $(window).scroll(function(){
+    //滾動軸的最頂點位置
+    let scrollPos = $(window).scrollTop();
+    //視窗高度
+    let windowHeight = $(window).height();
+    $('.scrollSpy').each(function(){
+      let target = $(this).attr('href');
+      let targetPos = $(target).offset().top;
+      let targetHeight = $(target).outerHeight();
+      if (targetPos-120 <= scrollPos && (targetPos + targetHeight) > scrollPos){
+        $('.scrollSpy').removeClass('active')
+        $(this).addClass('active');
+      }else {
+        $(this).removeClass('active');
+      }
+    });
+  });
+});
+
